46400675a1e74dfc98d591dccf3972bcb102a7e4
Carl Ahlers <wcarlahlers@gmail.com>
2026-05-19 02:30:22 -0400

    Introduce EngineState enum and update GameEngine
    Added enum for EngineState and updated member variables to recognize it.

--- core/GameEngine.h
+++ core/GameEngine.h
@@ -7,12 +7,20 @@
 
 namespace Engine {
 
-// Since PAL is inside Engine, we don't need ::PAL or PAL::
-// we can just use PAL:: directly because we are already in the Engine scope.
+// 1. DEFINITIONS FIRST
+enum class EngineState {
+    TitleScreen,
+    SongSelect,
+    Gameplay,
+    ResultsScreen
+};
 
+struct NoteState { uint8_t hitResult; };
+struct FrameResult { uint16_t perfectCount; uint16_t missCount; uint16_t holdDropCount; };
+
+// 2. CLASS DEFINITION SECOND
 class GameEngine {
 public:
-    // Lifecycle
     bool init(const PAL::PlatformBundle& bundle, uint8_t initialSongId);
     void tick();
     void render();
@@ -20,25 +28,21 @@ public:
     void shutdown();
 
 private:
-    // Core Engine Pointers
     PAL::GraphicsInterface* s_graphics = nullptr;
     PAL::AudioInterface*    s_audio    = nullptr;
     PAL::InputInterface*    s_input    = nullptr;
     PAL::ClockInterface*    s_clock    = nullptr;
 
-    // Simulation State
     bool        m_isRunning = false;
-    EngineState m_currentState;
+    EngineState m_currentState; // Now recognized
     uint8_t     m_selectedSong;
     uint16_t    m_score = 0;
     uint16_t    m_combo = 0;
     uint16_t    m_missCount = 0;
 
-    // Gameplay members
     void updateGameplaySimulation(PAL::InputState pressed);
-    FrameResult evaluateChart(PAL::FP16 trackPos, PAL::InputState pressed);
+    FrameResult evaluateChart(PAL::FP16 trackPos, PAL::InputState pressed); // Now recognized
     
-    // Helpers
     void resetPuzzleGrid();
     void loadChart(const NoteChart* chart);
     void resetScoreCounters();
@@ -48,7 +52,6 @@ private:
     void checkAndResolveMatches();
     void spawnBurst(uint8_t lane, uint8_t count);
     
-    // UI/Render helpers
     void renderTitleScreen() const;
     void renderSongSelectMenu() const;
     void renderGameplayScene();
